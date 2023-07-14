fdc96f53da6c38a56c9ee39663ca498197e427d7
MmeliNtshakala <mmeliroot@gmail.com>
2023-07-14 05:49:15 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: 'print alphabets in lowecase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
