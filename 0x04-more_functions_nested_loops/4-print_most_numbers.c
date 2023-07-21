86bcfb2de93f4a097dc0f666e467164cedb57b07
MmeliNtshakala <mmeliroot@gmail.com>
2023-07-21 13:16:09 -0700

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
  * print_most_numbers - prints 01356789
  * Return: void
  */
- 
  void print_most_numbers(void)
  {
      char m;
      for (m = '0'; m <= '9'; m++)
      {
-     if (!(m == '2' || m == '4'))
-     _putchar(m);
+         if (!(m == '2' || m == '4'))
+         _putchar(m);
      }
      _putchar('\n');
  }
