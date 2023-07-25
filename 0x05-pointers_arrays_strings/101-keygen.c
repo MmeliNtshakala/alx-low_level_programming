ed9c2d4a6d2df40c18a3dbaefbfd4366882e151a
MmeliNtshakala <mmeliroot@gmail.com>
2023-07-24 17:56:00 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,67 +3,22 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- *
- * Return: Always 0 (Success)
+ * main - generates random passwords for 101-crackme
+ * Return: Always 0
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
-
-	sum = 0;	
+	int sum;
+	char c;
 
 	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	while (sum <= 2645)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
+	putchar(2772 - sum);
 
 	return (0);
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int pass[100];
-	int i, sum, n;
-
-	sum = 0;	
-
-	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
-	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
-	}
-
-	return (0);
-}}
+}
