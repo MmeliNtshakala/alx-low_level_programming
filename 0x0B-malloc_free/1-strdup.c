da7ce62ca79bbc39b846752d90aaa48fbb2abdb3
MmeliNtshakala <mmeliroot@gmail.com>
2023-08-08 14:06:04 -0700

    strdup1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,49 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
+ * _strdup - duplicates string
+ * @str: string to duplicate
  *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * Return: a pointer to the duplicated string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_strdup(char *str)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *newString;
+	unsigned int i, size;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
+	size = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
 
-	sout[i] = '\0';
+	newString = (char *) malloc(sizeof(char) * (size + 1));
 
-	return (sout);
+	if (newString == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		newString[i] = str[i];
+	newString[size] = '\0';
+
+	return (newString);
 }
