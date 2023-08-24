6cccc4c15abe1c0bed6a90d0c632300f2424bb77
MmeliNtshakala <mmeliroot@gmail.com>
2023-08-24 16:51:00 -0700

    LIST LEN

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
-
 /**
- * lists -number of elements in list
- * @h: pointer to struct
- * Return : length
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
+
 size_t list_len(const list_t *h)
 {
+	size_t nelem;
 
-unsigned int i = 0;
-
+	nelem = 0;
 	while (h != NULL)
 	{
-		i++;
 		h = h->next;
+		nelem++;
 	}
-	return (i);
+	return (nelem);
 }
