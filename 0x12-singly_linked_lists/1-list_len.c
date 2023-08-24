e19b45f7fea85ce9ede72cb66d9967e8b92c227c
MmeliNtshakala <mmeliroot@gmail.com>
2023-08-24 16:24:11 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * lists -number of elements in list
+ * @h: pointer to struct
+ * Return : length
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
