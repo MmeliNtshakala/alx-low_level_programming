3114af95bc675de393b7e9e26b0ccbce1f6ed989
MmeliNtshakala <mmeliroot@gmail.com>
2023-10-02 20:16:09 -0700

    create txt file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,17 +9,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int y;
-	ssize_t x;
+	int fd, y, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	y = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (y == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	y = write(fd, text_content, len);
+	if (fd == -1 || y == -1)
 		return (-1);
-	x = write(y, text_content, strlen(text_content));
-	if (x == -1)
-		return (-1);
-	close(y);
+	close(fd);
 	return (1);
 }
