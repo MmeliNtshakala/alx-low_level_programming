2e80ad094f95f98390153a01c6707cac90902a4c
MmeliNtshakala <mmeliroot@gmail.com>
2023-10-26 09:59:45 -0700

    commit 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * binary_to_uit- converts a binary to usigned unit
+ * @b: binary number
+ *
+ * Return: number
+ */
+
+unsigned int binary_to_uit(const char *b)
+{
+	int x;
+	unsigned int value = 0;
+
+	if (!b)
+		return (0);
+
+	for (x = 0; b[x]; x++)
+	{
+		if (b[x] < '0' || b[x] > '1')
+			return (0);
+		value = 2 * value + (b[x] - '0');
+	}
+	return (value);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * print_binary - integer to binary
+ * @x: unsigned int
+ *
+ * Return: binary
+ */
+
+void print_binary(unsigned long int x)
+{
+	if (x > 1)
+		print_binary(x >> 1);
+	_putchar((x & 1) + '0');
+}
