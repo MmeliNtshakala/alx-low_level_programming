1bc031a9a36cbb265f399009cb2f7e658404ec0b
MmeliNtshakala <mmeliroot@gmail.com>
2023-10-26 10:19:58 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  * binary_to_uit- converts a binary to usigned unit
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  * print_binary - integer to binary
