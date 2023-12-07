0d0a9e5b910658fc622a99fddf3904718bc4718f
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-07 06:15:49 -0800

    length of list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in lists dlistint_t
+ * @h: The head of dlistint_t list
+ *
+ * Return: returns number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+
+	while (h != NULL)
+	{
+		length++;
+		h = h->next;
+	}
+	return (length);
